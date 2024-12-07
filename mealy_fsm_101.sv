7221758059bcc91d0f8e022e6871a6259cb38503
Abhishek Chakraborty <abhishek.chakrab0001@gmail.com>
2024-12-07 21:06:18 +0530

    modified the code base for sequence detector.

--- mealy_fsm_101.sv
+++ mealy_fsm_101.sv
@@ -9,20 +9,16 @@ parameter s1 = 1;
 parameter s2 = 2;
 
 reg [1:0] state, nstate;
-
 //---------------------------------
 //------------RESET_LOGIC----------
 //---------------------------------
-
 always @(posedge clk) begin
     if(rst) state <= idle;
     else state <= nstate;
 end
-
 //---------------------------------
 //------------OUTPUT_LOGIC---------
 //---------------------------------
-
 always @(state,din) begin
     case(state)
         idle : begin
@@ -38,11 +34,9 @@ always @(state,din) begin
        default : dout = 'd0;
     endcase
 end
-
 //---------------------------------
 //------------NST------------------
 //---------------------------------
-
 always @(state,din) begin
     case(state)
         idle : begin
